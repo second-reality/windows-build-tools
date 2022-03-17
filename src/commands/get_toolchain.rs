9f3e18ab62ba623164e792e92e4be22e837ba02e
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-03-17 21:51:39 +0100

    get-toolchain: add required arg for version and install dir

--- src/commands/get_toolchain.rs
+++ src/commands/get_toolchain.rs
@@ -1,8 +1,12 @@
 use super::catalog::{get_catalog, CatalogIndex};
+use log::info;
 
-pub fn run() {
+pub fn run(toolchain_version: String, install_dir: String) {
     let catalog = get_catalog();
     let index = CatalogIndex::new(&catalog);
+
+    info!("get toolchain version {toolchain_version} in {install_dir}");
+
     assert!(index.get_package("hi!").is_none());
     todo!();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate clap;
 
-use clap::Command;
+use clap::{Arg, Command};
 mod commands;
 
 const LIST_PACKAGES: &str = "list-packages";
 const LIST_TOOLCHAINS: &str = "list-toolchains";
 const GET_TOOLCHAIN: &str = "get-toolchain";
+
+const ARG_TOOLCHAIN_VERSION: &str = "toolchain-version";
+const ARG_INSTALL_DIR: &str = "install-dir";
 
 fn main() {
     env_logger::Builder::from_default_env()
@@ -17,15 +20,22 @@ fn main() {
         .subcommand_required(true)
         .subcommand(Command::new(LIST_PACKAGES))
         .subcommand(Command::new(LIST_TOOLCHAINS))
-        .subcommand(Command::new(GET_TOOLCHAIN))
+        .subcommand(
+            Command::new(GET_TOOLCHAIN)
+                .arg(Arg::new(ARG_TOOLCHAIN_VERSION).required(true))
+                .arg(Arg::new(ARG_INSTALL_DIR).required(true)),
+        )
         .get_matches();
 
-    let (command, _) = m.subcommand().expect("supposed to be required");
+    let (command, args) = m.subcommand().expect("supposed to be required");
 
     match command {
         LIST_PACKAGES => commands::list_packages::run(),
         LIST_TOOLCHAINS => commands::list_toolchains::run(),
-        GET_TOOLCHAIN => commands::get_toolchain::run(),
+        GET_TOOLCHAIN => commands::get_toolchain::run(
+            args.value_of_t_or_exit(ARG_TOOLCHAIN_VERSION),
+            args.value_of_t_or_exit(ARG_INSTALL_DIR),
+        ),
         _ => panic!("subcommand {command} not expected"),
     }
 }
