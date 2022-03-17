5b8b92fc8c0dad299f592beba035d24ac565d18b
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2022-03-17 21:12:03 +0100

    turn command names into constants

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate clap;
 use clap::Command;
 mod commands;
 
+const LIST_PACKAGES: &str = "list-packages";
+const LIST_TOOLCHAINS: &str = "list-toolchains";
+const GET_TOOLCHAIN: &str = "get-toolchain";
+
 fn main() {
     env_logger::Builder::from_default_env()
         .filter_level(log::LevelFilter::Info)
@@ -11,17 +15,17 @@ fn main() {
     let m = Command::new("wbt")
         .about("Use windows build tools")
         .subcommand_required(true)
-        .subcommand(Command::new("list-packages"))
-        .subcommand(Command::new("list-toolchains"))
-        .subcommand(Command::new("get-toolchain"))
+        .subcommand(Command::new(LIST_PACKAGES))
+        .subcommand(Command::new(LIST_TOOLCHAINS))
+        .subcommand(Command::new(GET_TOOLCHAIN))
         .get_matches();
 
     let (command, _) = m.subcommand().expect("supposed to be required");
 
     match command {
-        "list-packages" => commands::list_packages::run(),
-        "list-toolchains" => commands::list_toolchains::run(),
-        "get-toolchain" => commands::get_toolchain::run(),
+        LIST_PACKAGES => commands::list_packages::run(),
+        LIST_TOOLCHAINS => commands::list_toolchains::run(),
+        GET_TOOLCHAIN => commands::get_toolchain::run(),
         _ => panic!("subcommand {command} not expected"),
     }
 }
